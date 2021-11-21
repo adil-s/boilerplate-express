d66a08c3b49887018b67fd4d8e4c1f37017cbada
adil-s <ah.shaikh97@gmail.com>
2021-11-21 20:12:40 +0530

    fixing time foramt add 20 seconds

--- myApp.js
+++ myApp.js
@@ -11,11 +11,14 @@ app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"))
 app.get("/json", (req, res) => res.json({ "message": "hello json" }))
 app.get("/json/v3", (req, res) => res.json({ "msg": "hello json" }))
 app.get("/now", (req, res, next) => {
-        req.time =  new Date().toDateString()
-        next()
-    },
-    function(req, res){
-        return res.json({'time': req.time})
+    let now = new Date()
+    now.setSeconds(new Date().getSeconds() + 20)
+
+    req.time = now;
+    next()
+},
+    function (req, res) {
+        return res.json({ 'time': req.time.toDateString() })
     }
 )
 
