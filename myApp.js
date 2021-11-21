965f5c938ed0ff52038eed29fd628419f602ee07
Adilhusain Shaikh <ah.shaikh97@gmail.com>
2021-11-21 18:15:01 +0530

    fix middle ware for spacing

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"))
 app.get("/json", (req, res) => res.json({ "message": "hello json" }))
 app.get("/json/v3", (req, res) => res.json({ "msg": "hello json" }))
 app.use((req, rres, next) => {
-    console.log(req.method, req.path,  "-",  req.ip);
+    console.log(req.method, " ", req.path, " - ", req.ip);
     next();
 })
 
