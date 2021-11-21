74b2c09c45af3e0779ebd219202df477e5434b16
adil-s <ah.shaikh97@gmail.com>
2021-11-22 01:22:18 +0530

    bodyparser challenge

--- myApp.js
+++ myApp.js
@@ -1,12 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
 
-app.use((req, rres, next) => {
-    //console.log(`$req.method, req.path, " - ", req.ip`);
+app.use((req, res, next) => {
     console.log(`${req.method} ${req.path} - ${req.ip}`)
     next();
 })
-
+app.use(bodyParser.urlencoded({ 'extended': 'false' }))
+app.use(bodyParser.json())
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"))
 app.get("/json", (req, res) => res.json({ "message": "hello json" }))
 app.get("/json/v3", (req, res) => res.json({ "msg": "hello json" }))
@@ -22,10 +23,10 @@ app.get("/now", (req, res, next) => {
     }
 )
 app.get("/:word/echo", (req, res) => {
-    return res.json({'echo': req.params.word});
+    return res.json({ 'echo': req.params.word });
 })
-app.route("/name").get ((req, res) => {
-    return res.json  ({'name':`${req.query.first} ${req.query.last}`})
+app.route("/name").get((req, res) => {
+    return res.json({ 'name': `${req.query.first} ${req.query.last}` })
 })
 
 
