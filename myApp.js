9d799446f297823958d84b78800dbf491da3153d
Adilhusain Shaikh <ah.shaikh97@gmail.com>
2021-11-21 17:43:57 +0530

    adding /json/v3 root

--- myApp.js
+++ myApp.js
@@ -3,6 +3,7 @@ var app = express();
 
 app.get("/", (req, res) => res.sendFile (__dirname + "/views/index.html"))
 app.get ("/json", (req, res)=> res.json({"message": "hello json"}))
+app.get ("/json/v3", (req, res)=> res.json({"msg": "hello json"}))
 
 
 
