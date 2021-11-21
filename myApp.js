9e25187145a798b51ebdfd22b9664dec9ede283c
Adilhusain Shaikh <ah.shaikh97@gmail.com>
2021-11-21 14:02:39 +0530

    added /json root

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get("/", (req, res) => res.sendFile (__dirname + "/views/index.html"))
-
+app.get ("/json", (req, res)=> res.json({"message": "hello json"}))
 
 
 
