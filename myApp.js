0c4879ea2e26f78df16ff12f19f9e2e5f6a5928b
Adilhusain Shaikh <ah.shaikh97@gmail.com>
2021-11-21 18:51:07 +0530

    middleware format fixed

--- myApp.js
+++ myApp.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.use((req, rres, next) => {
+    //console.log(`$req.method, req.path, " - ", req.ip`);
+    console.log(`${req.method} ${req.path} - ${req.ip}`)
+    next();
+})
+
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"))
 app.get("/json", (req, res) => res.json({ "message": "hello json" }))
 app.get("/json/v3", (req, res) => res.json({ "msg": "hello json" }))
-app.use((req, rres, next) => {
-    console.log(req.method, " ", req.path, " - ", req.ip);
-    next();
-})
 
 
 
