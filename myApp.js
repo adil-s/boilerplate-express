0581f24e3c96db9f4545698816bb0a99d8fee957
adil-s <ah.shaikh97@gmail.com>
2021-11-21 19:14:25 +0530

    adding time middleware

--- myApp.js
+++ myApp.js
@@ -10,7 +10,14 @@ app.use((req, rres, next) => {
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"))
 app.get("/json", (req, res) => res.json({ "message": "hello json" }))
 app.get("/json/v3", (req, res) => res.json({ "msg": "hello json" }))
-
+app.get("/now", (req, res, next) => {
+        req.time =  new Date().toDateString()
+        next()
+    },
+    function(req, res){
+        return res.json({'time': req.time})
+    }
+)
 
 
 
