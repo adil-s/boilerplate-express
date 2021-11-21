7e770c6d3c87d7956266ca7706fbf855cdbf07c9
Adilhusain Shaikh <ah.shaikh97@gmail.com>
2021-11-21 18:07:27 +0530

    middle ware challange

--- myApp.js
+++ myApp.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get("/", (req, res) => res.sendFile (__dirname + "/views/index.html"))
-app.get ("/json", (req, res)=> res.json({"message": "hello json"}))
-app.get ("/json/v3", (req, res)=> res.json({"msg": "hello json"}))
-
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"))
+app.get("/json", (req, res) => res.json({ "message": "hello json" }))
+app.get("/json/v3", (req, res) => res.json({ "msg": "hello json" }))
+app.use((req, rres, next) => {
+    console.log(req.method, req.path,  "-",  req.ip);
+    next();
+})
 
 
 
